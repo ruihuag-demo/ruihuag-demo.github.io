a1bb3b87ebd36cdc4384c2f35610730abb88d3f1
guanruihua <2116047353@qq.com>
2025-05-20 15:19:15 +0800

    save

---
+++ .conf/index.mjs
@@ -0,0 +1,65 @@
+import fs from 'fs/promises'
+import path from 'path'
+
+const ig = ['node_modules', '.conf', '.git', 'bg', '.assets', '.docs', '.husky', '.vscode', 'dev', 'Javascript']
+
+async function readDirectory(dir) {
+  // const db = {}
+  async function traverse(currentPath) {
+    const results = []
+    const files = await fs.readdir(currentPath)
+    for (const file of files) {
+      if (ig.includes(file)) continue
+      try {
+        const fullPath = path.join(currentPath, file)
+        const stat = await fs.stat(fullPath)
+        if (stat.isDirectory()) {
+          results.push({
+            name: file,
+            dir: true,
+            file: false,
+            child: await traverse(fullPath),
+          })
+        } else {
+          if (!/\.html$/.test(file)) {
+            continue
+          }
+          // if (/\.md$/.test(file)) {
+          // const content = await (
+          //   await fs.readFile(fullPath, 'utf-8')
+          // ).replace(/\s+/g, '')
+          // db[fullPath] = content
+
+          results.push({
+            dir: false,
+            file: true,
+            name: file,
+            path: fullPath,
+          })
+          // }
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    return results
+  }
+
+  const depth1 = await traverse(dir)
+
+  return [
+    {
+      name: dir,
+      dir: true,
+      file: false,
+      child: depth1,
+    },
+    // db,
+  ]
+}
+
+readDirectory('./').then(([files, db]) => {
+  // fs.writeFile('./tree.json', JSON.stringify(files, null, 2)),
+  fs.writeFile('./.assets/files-tree.json', JSON.stringify(files))
+  // fs.writeFile('.//db.json', JSON.stringify(db))
+})
--- config/script/pre-commit.js
+++
@@ -1,5 +0,0 @@
-console.log('------------------------------ ')
-console.log('pre-commit.js start running....')
-
-console.log('pre-commit.js end running....')
-console.log('------------------------------ ')
